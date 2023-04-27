9327a3e379e0a0900a0ae155671b43145eba40db
github-main-user <kolya.skripn@mail.ru>
2023-04-27 21:33:01 +0300

    change wins to pads

--- src/display.c
+++ src/display.c
@@ -1,11 +1,15 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <curses.h>
+#include <ncurses.h>
 
 #define BYTES_WIDTH 16
 #define ADDR_LENGTH 8
 
-static WINDOW *addr_win, *hex_win, *ascii_win;
+int get_file_size(void);
+
+static WINDOW *addr_pad, *hex_pad, *ascii_pad;
+static int curr_y = 0;
+static int max_lines;
 
 void curses_init()
 {
@@ -15,15 +19,15 @@ void curses_init()
 	noecho();
 	keypad(stdscr, TRUE);
 
+	max_lines = get_file_size() / BYTES_WIDTH;
 	
-	addr_win =  newwin(LINES, ADDR_LENGTH,     0, 0);
-	hex_win =   newwin(LINES, BYTES_WIDTH * 3, 0, ADDR_LENGTH + 2);
-	ascii_win = newwin(LINES, BYTES_WIDTH,     0, (ADDR_LENGTH + 2) + (BYTES_WIDTH * 3) + 1);
+	//addr_pad =  newpad(LINES, ADDR_LENGTH,     0, 0);
+	//hex_pad =   newpad(LINES, BYTES_WIDTH * 3, 0, ADDR_LENGTH + 2);
+	//ascii_pad = newpad(LINES, BYTES_WIDTH,     0, (ADDR_LENGTH + 2) + (BYTES_WIDTH * 3) + 1);
 
-	scrollok(addr_win, TRUE);
-	scrollok(hex_win, TRUE);
-	scrollok(ascii_win, TRUE);
-
+	addr_pad =  newpad(max_lines, ADDR_LENGTH);
+    hex_pad =   newpad(max_lines, BYTES_WIDTH * 3);
+    ascii_pad = newpad(max_lines, BYTES_WIDTH);
 
 	refresh();
 }
@@ -31,35 +35,44 @@ void curses_init()
 void set_addr_text(int size)
 {
 	for (int i = 0; i < size; i += BYTES_WIDTH)
-		wprintw(addr_win, "%08x", i);
+		wprintw(addr_pad, "%08x", i);
 
-	wrefresh(addr_win);
+	wrefresh(addr_pad);
 }
 
 void set_hex_text(uint8_t *bytes, int size)
 {
 	for (int i = 0; i < size; ++i)
-		wprintw(hex_win, "%02x ", bytes[i]);	
+		wprintw(hex_pad, "%02x ", bytes[i]);	
 
-	wrefresh(hex_win);
+	wrefresh(hex_pad);
 }
 
 void set_ascii_text(uint8_t *bytes, int size)
 {
 	for (int i = 0; i < size; ++i)
-		wprintw(ascii_win, "%c", (bytes[i] > 32 && bytes[i] < 128) ? bytes[i] : '.');
+		wprintw(ascii_pad, "%c", (bytes[i] > 32 && bytes[i] < 128) ? bytes[i] : '.');
 
-	wrefresh(ascii_win);
+	wrefresh(ascii_pad);
 }
 
 void curses_exit(void)
 {
-	delwin(addr_win);
-	delwin(hex_win);
-	delwin(ascii_win);
+	delwin(addr_pad);
+	delwin(hex_pad);
+	delwin(ascii_pad);
 
 	curs_set(1);
 	endwin();
+}
+
+static void scroll_all_wins(int key, int direction)
+{
+	curr_y += direction;
+
+	prefresh(addr_pad,  curr_y, 0, 0, 0, max_lines, ADDR_LENGTH);
+	prefresh(hex_pad,   curr_y, 0, 0, ADDR_LENGTH + 2, max_lines, BYTES_WIDTH);
+	prefresh(ascii_pad, curr_y, 0, 0, (ADDR_LENGTH + 2) + (BYTES_WIDTH * 3) + 1, max_lines, BYTES_WIDTH * 3);
 }
 
 void control()
@@ -71,10 +84,14 @@ void control()
 		switch (key = getch())
 		{
 			case 'q': curses_exit(); exit(0); break;
+			case 'h':
 			case KEY_LEFT: break;
+			case 'l':
 			case KEY_RIGHT: break;
-			case KEY_UP: break;
-			case KEY_DOWN: break;
+			case 'k':
+			case KEY_UP: scroll_all_wins(key, 1); break;
+			case 'j': 
+			case KEY_DOWN: scroll_all_wins(key, -1); break;
 		}
 	}
 }
