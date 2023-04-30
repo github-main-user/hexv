0bedc665338660bfecd460cf7b1ea177ddf87159
github-main-user <kolya.skripn@mail.ru>
2023-04-30 22:43:02 +0300

    scroll work, bad boundary handle

--- src/display.c
+++ src/display.c
@@ -4,12 +4,15 @@
 
 #define BYTES_WIDTH 16
 #define ADDR_LENGTH 9
+#define max(A, B) (A > B) ? A : B
+#define min(A, B) (A > B) ? B : A 
 
 extern int get_file_size(void);
 
 static WINDOW *addr_pad, *hex_pad, *ascii_pad;
-static int curr_y = 0;
+static int cursor = 0, curr_y = 0;
 static int max_lines;
+void move_cursor(int);
 
 void curses_init()
 {
@@ -37,6 +40,7 @@ void init_pallete()
 	init_pair(0, COLOR_WHITE, COLOR_BLACK);
 	init_pair(1, COLOR_BLACK, COLOR_WHITE);
 	init_pair(2, COLOR_CYAN, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
 }
 
 void print_header()
@@ -44,7 +48,6 @@ void print_header()
 	int i;
 
 	attron(COLOR_PAIR(2));
-
 	printw("offset:   ");
 	for (i = 0; i < BYTES_WIDTH; ++i)
 		printw("%02x ", i);
@@ -52,7 +55,6 @@ void print_header()
 	addch(' ');
 	for (i = 0; i < BYTES_WIDTH; ++i)
 		printw("%1x", i);
-
 	attroff(COLOR_PAIR(2));
 }
 
@@ -67,10 +69,10 @@ void fill_all_pads(uint8_t *bytes, int size)
 {
 	int i;
 
-	attron(COLOR_PAIR(2));
+	wattron(addr_pad, COLOR_PAIR(3));
 	for (i = 0; i < size; i += BYTES_WIDTH)
 		wprintw(addr_pad, "%08x:", i);
-	attroff(COLOR_PAIR(2));
+	wattron(addr_pad, COLOR_PAIR(3));
 
 	for (i = 0; i < size; ++i)
 		wprintw(hex_pad, "%02x ", bytes[i]);	
@@ -80,6 +82,7 @@ void fill_all_pads(uint8_t *bytes, int size)
 
 
 	refresh_all_pads();
+	move_cursor(0); // color first bytes;
 }
 
 void curses_exit(void)
@@ -92,9 +95,19 @@ void curses_exit(void)
 	endwin();
 }
 
-static void scroll_all_wins(int key, int direction)
+void move_cursor(int direction)
 {
-	curr_y += direction;
+	cursor = max(0, cursor + direction);
+	if ((cursor / BYTES_WIDTH) >= LINES - 1)
+		curr_y++;
+
+	wchgat(hex_pad, 2, A_NORMAL, 0, NULL); // clear old
+	wchgat(ascii_pad, 1, A_NORMAL, 0, NULL); // clear old
+
+	wmove(hex_pad, cursor / BYTES_WIDTH, cursor % BYTES_WIDTH * 3);
+	wmove(ascii_pad, cursor / BYTES_WIDTH, cursor % BYTES_WIDTH);
+	wchgat(hex_pad, 2, A_REVERSE, 0, NULL);	
+	wchgat(ascii_pad, 1, A_REVERSE, 0, NULL);	
 
 	refresh_all_pads();
 }
@@ -109,13 +122,13 @@ void control()
 		{
 			case 'q': curses_exit(); exit(0); break;
 			case 'h':
-			case KEY_LEFT: break;
+			case KEY_LEFT: move_cursor(-1); break;
 			case 'l':
-			case KEY_RIGHT: break;
+			case KEY_RIGHT: move_cursor(1); break;
 			case 'k':
-			case KEY_UP: scroll_all_wins(key, -1); break;
+			case KEY_UP: move_cursor(-BYTES_WIDTH); break;
 			case 'j': 
-			case KEY_DOWN: scroll_all_wins(key, 1); break;
+			case KEY_DOWN: move_cursor(BYTES_WIDTH); break;
 		}
 	}
 }
